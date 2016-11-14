113c4f51ec39799a9d44b54970c52973ed1ffd4a
taewankung <taewankung@gmail.com>
2016-11-15 01:41:39 +0700

    add hero2 and enemy_sensor

---
+++ naga/managers/unit/sensor/enemy_sensor.py
@@ -0,0 +1,26 @@
+from .sensor import Sensor
+import math
+class EnemySensor(Sensor):
+
+    def __init__(self,unit,unit_list):
+        super().__init__(unit,unit_list)
+
+    def scan(self,in_range = 50):
+        unit_in_range = list()
+        #u is unit
+        for u in self.unit_list:
+            dist_u = math.sqrt(
+                          pow((u.pos_x - self.unit.pos_x),2)+
+                          pow((u.pos_y - self.unit.pos_y),2)
+                          )
+            if dist_u <= in_range:
+                if u not in unit_in_range:
+                    unit_in_range.append(u.name)
+            else:
+                if u in unit_in_range:
+                    unit_in_range.remove(u.name)
+        return unit_in_range
+
+    def update_unit_list(self):
+        self.unit_list = self.unit.enemy_list
+        pass
