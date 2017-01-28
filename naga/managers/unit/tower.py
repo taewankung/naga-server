8e0b9a1b43b48b1b63e175277c2da5521bc3d287
taewankung <taewankung@gmail.com>
2017-01-28 21:51:41 +0700

    fixed unit

--- naga/managers/unit/tower.py
+++ naga/managers/unit/tower.py
@@ -26,3 +26,23 @@ class Tower(Building):
             if time.time()-self.current_speed_dmg >= self.damage_speed:
                 self.near_enemy_list[0].current_hp = self.near_enemy_list[0].current_hp - self.damage
                 self.current_speed_dmg = time.time()
+
+    def to_data_dict(self):
+        result = dict(id=self.id,
+                name=self.name,
+                max_hp=self.max_hp,
+                current_hp=self.current_hp,
+                hp_regen=self.hp_regen,
+                max_mana=self.max_mana,
+                current_mana=self.current_mana,
+                mana_regen=self.mana_regen,
+                damage=self.damage,
+                armor=self.armor,
+                take_damaged=self.take_damaged,
+                buff_status=self.buff_status,
+                pos_x=self.pos_x,
+                pos_y=self.pos_y,
+                range=self.range,
+                id_controller=self.id_controller,
+                alive=self.alive,)
+        return result
